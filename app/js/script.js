76d10bd38051219ae12e28e3489fa142a088397f
leticiaborchardt <leticiaborchardt123@gmail.com>
2023-02-08 16:53:17 -0300

    responsive navbar for mobile

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,21 @@
-console.log('hello');
+const navbarMobile = document.getElementById('navbar-mobile');
+const overlayBackground = document.getElementById('overlay');
+
+const hideNavbar = () => {
+    navbarMobile.style.width = '0';
+    overlayBackground.classList.remove("overlay--show");
+}
+
+function openNavbar() {
+    navbarMobile.style.width = '70%';
+    overlayBackground.classList.add("overlay--show");
+
+    overlayBackground.addEventListener('click', (event) => {
+        hideNavbar();
+    });
+}
+
+
+function closeNavbar() {
+    hideNavbar();
+}
